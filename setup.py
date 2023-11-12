344ec8de3adead75062dc8667f650c19d9ba2d1b
ipritom <pritom.blue2@gmail.com>
2023-11-12 15:55:39 +0600

    pip install from github

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+VERSION = "0.1.0"
+
+setup(
+    name="fletFlow",
+    version=VERSION,
+    description="Tiny Framework for Flet based application development with ease",
+    url="https://github.com/ipritom/fletFlow",
+    author='Pritom Mojumder',
+    author_email='pritom.blue2@gmail.com',
+    license='MIT',
+    packages=['fletSDP'],
+    install_requires=[
+        "flet>=0.11.0",
+    ],
+    python_requires='>=3.9',
+    zip_safe=False
+)
